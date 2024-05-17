af27d681eea487a064a49b2b854af6fbe832b389
jon <zhenagajo234@gmail.com>
2024-05-17 09:36:35 +0700

    add summax AC sol & bruteforce sol

---
+++ problems/summax/solution.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+const int INF = 1e18;
+
+int squared(int n)
+{
+    return n * n;
+}
+
+int32_t main()
+{
+
+    int N, K;
+    cin >> N >> K;
+    int arr[N + 1]; // 1-based indexing
+
+    for (int i = 1; i <= N; i++)
+        cin >> arr[i];
+
+    int prefix_sum[N + 1] = {0};
+    for (int i = 1; i <= N; i++)
+        prefix_sum[i] = prefix_sum[i - 1] + arr[i];
+
+    int dp[N + 1][K + 1];
+
+    for (int i = 0; i <= N; i++)
+        for (int j = 0; j <= K; j++)
+            dp[i][j] = -INF;
+
+    dp[1][0] = arr[1];
+    dp[1][1] = squared(arr[1]);
+
+    for (int i = 2; i <= N; i++)
+    {
+        for (int j = 0; j <= K; j++)
+        {
+            for (int k = 0; k <= j; k++)
+            {
+                if(k == 0){
+                    dp[i][j] = max(dp[i][j],arr[i] + dp[i-1][j]);
+                }
+                else if(k == i){
+                    dp[i][j] = max(dp[i][j],squared(prefix_sum[i]-prefix_sum[i-k]) + dp[i-k][j-k]);
+                }else{
+                    dp[i][j] = max(dp[i][j],squared(prefix_sum[i]-prefix_sum[i-k]) + dp[i-k-1][j-k]);
+                }
+            }
+        }
+    }
+
+    cout << dp[N][K]
+;
+    return 0;
+}
