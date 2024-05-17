74364ea6fdf5032db4e1d8ddfe06a07a486c30dc
Farizki Kurniawan <fariski.mail@gmail.com>
2024-05-17 15:05:29 +0700

    solution stringkeapus

---
+++ problems/String-Keapus/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+set<int> occ[26];
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    for(int i=0; i<s.length(); i++){
+        int alp = s[i] - 'a';
+        occ[alp].insert(i);
+    }
+
+    int ans = 1;
+    int curIdx = -1;
+
+    for(int i=0; i<t.length(); i++){
+        int alp = t[i] - 'a';
+        auto it = occ[alp].upper_bound(curIdx);
+
+        // ada
+        if (it != occ[alp].end()){
+            curIdx = *it;
+        }
+
+        // gk ada
+        else if(occ[alp].size() > 0){
+            curIdx = *occ[alp].begin();
+            ans += 1;
+        }
+        else{
+            ans = -1;
+            break;
+        }
+    }
+
+    cout<<ans<<endl;
+    
+}
